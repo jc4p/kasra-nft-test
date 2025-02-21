63caab2f5e80c246dc52ff54fd17b71ac1af964e
Kasra Rahjerdi <hi@kasra.codes>
2025-02-20 22:01:13 -0800

    Initial Commit

---
+++ main.js
@@ -0,0 +1,8 @@
+document.getElementById('mintButton').addEventListener('click', async () => {
+    try {
+        // Mint functionality will go here
+        console.log('Mint button clicked');
+    } catch (error) {
+        console.error('Error during minting:', error);
+    }
+}); 

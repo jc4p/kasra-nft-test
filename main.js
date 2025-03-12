d51dfd30c1bedf67e3ef918b23188ae300f71807
Kasra Rahjerdi <hi@kasra.codes>
2025-03-11 19:47:41 -0700

    Add test share button

--- main.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('mintButton').addEventListener('click', async () => {
-    try {
-        // Mint functionality will go here
-        console.log('Mint button clicked');
-    } catch (error) {
-        console.error('Error during minting:', error);
-    }
-}); 
